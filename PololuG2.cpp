edf22dc077c2fdc8d27e9f69f4f4dd3f04b95d76
Allen Izquierdo <izquia1@unlv.nevada.edu>
2017-12-30 17:29:31 -0800

    Add MiniMaestroService to PololuG2
    PololuG2 can be constructed for maestro now.

--- PololuG2.cpp
+++ PololuG2.cpp
@@ -11,11 +11,41 @@ PololuG2::PololuG2(const byte enable_pin, const byte pwm_pin, const byte dir_pin
 	pinMode(dir_pin, OUTPUT);
 }
 
+PololuG2::PololuG2(const MiniMaestroService &miniMaestroService, const byte enable_channel, const byte pwm_channel, const byte dir_channel) : Motor(MOTOR_ID_POLOLUG2)
+{
+	this->enable_pin = enable_channel;
+	this->pwm_pin = pwm_channel;
+	this->dir_pin = dir_channel;
+	this->miniMaestroService = &miniMaestroService;
+
+	maestroService = true;
+}
+
 // TODO: Implement a way to detect if PololuG2 motor controller is powered before sending signals.
 void PololuG2::setPower(const float power)
 {
 
 	Motor::setPower(power);
+	if(maestroService)
+	{
+		if(this->getPower() < 0)
+		{
+			miniMaestroService->queTarget(dir_pin, 0);
+		} else {
+			miniMaestroService->queTarget(dir_pin, 8000);
+		}
+
+		// abs() don't work on floats
+		if(getPower() < 0)
+		{
+			this->maestroOutput = uint16_t(-getPower() * 12000);
+		} else {
+			this->maestroOutput = uint16_t(getPower() * 12000);
+		}
+		miniMaestroService->queTarget(this->pwm_pin, maestroOutput);
+		return; // TODO: Find a cleaner way to seperate these two protocals (maestro vs pins).
+	}
+
 	if(this->getPower() < 0)
 	{
 		digitalWrite(dir_pin, LOW);
--- PololuG2.h
+++ PololuG2.h
@@ -2,16 +2,20 @@
 #define POLOLUG2_H
 #include <Arduino.h>
 #include <Motor.h>
+#include <MiniMaestroService.h>
 
 class PololuG2: public Motor {
 	public:
 		PololuG2(const byte enable_pin, const byte pwm_pin, const byte dir_pin);
+		PololuG2(const MiniMaestroService &miniMaestroService, const byte enable_channel, const byte pwm_channel, const byte dir_channel);
 		
 		// Setters and Getters
 		void setPower(const float power);
-		byte output;
 	protected:
 	private:
+		unsigned int maestroOutput;
+		bool maestroService;
+		MiniMaestroService* miniMaestroService; 
 		byte enable_pin;
 		byte pwm_pin;
 		byte dir_pin;
